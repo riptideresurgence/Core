a998c15cbca13c39f7e029b2767a5f3e1b7f1c9c
cutymeoRBLX <hkhanhnam81@gmail.com>
2023-11-12 00:17:44 +0700

    compile

--- dist/roblox.js
+++ dist/roblox.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getNameRepresentationFromInfo = exports.getUserInfo = void 0;
+exports.getUserIdFromUsername = exports.getNameRepresentationFromInfo = exports.getUserInfo = void 0;
 const noblox_js_1 = __importDefault(require("noblox.js"));
 async function getUserInfo(user) {
     let userId = typeof user == "string" ? await noblox_js_1.default.getIdFromUsername(user) : user;
@@ -14,3 +14,7 @@ function getNameRepresentationFromInfo(userInfo) {
     return userInfo.username == userInfo.displayName ? userInfo.username : `${userInfo.displayName} (@${userInfo.username})`;
 }
 exports.getNameRepresentationFromInfo = getNameRepresentationFromInfo;
+async function getUserIdFromUsername(username) {
+    return await noblox_js_1.default.getIdFromUsername(username);
+}
+exports.getUserIdFromUsername = getUserIdFromUsername;
